01677965d2efe5223e940c852890e6ebe1212946
Grace Burns <graceburns@chicagobooth.edu>
2023-04-25 15:16:49 +0000

    final

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,13 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  def movie
+    m_id = self.movie_id
+
+    matching_movie = Movie.where({ :id => m_id})
+
+    the_movie = matching_movie.at(0)
+
+    return the_movie
+  end
 end
